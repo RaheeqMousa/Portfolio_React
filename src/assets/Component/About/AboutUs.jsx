03e200cca20bf7a900f7fc70336a8b1e08455b8d
RaheeqMousa <raheeqmousa00@gmail.com>
2025-03-28 17:28:00 +0200

    Update AboutUs.jsx

--- src/assets/Component/About/AboutUs.jsx
+++ src/assets/Component/About/AboutUs.jsx
@@ -14,7 +14,7 @@ export default function AboutUs() {
             <div className={`${style.about}`}>
                   
                 <p className={`${ style.paragraphStyle}`}>
-                    Hi, I'm [Your Name], a passionate web developer with experience in building responsive and user-friendly websites. I specialize in front-end development using technologies like React, HTML, CSS, and JavaScript to create engaging digital experiences. Whether it's an eCommerce platform or a personal project, I focus on clean code, seamless performance, and intuitive design to bring ideas to life                
+                    Hi, I'm Raheeq Mousa, a passionate web developer with experience in building responsive and user-friendly websites. I specialize in front-end development using technologies like React, HTML, CSS, and JavaScript to create engaging digital experiences. Whether it's an eCommerce platform or a personal project, I focus on clean code, seamless performance, and intuitive design to bring ideas to life                
                 </p>
                 
             </div>
